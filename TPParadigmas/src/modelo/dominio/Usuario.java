b22af6290653f0c94c8702fdc8c43143b7ef7e40
franc <franc@FRANCO-NB>
2017-10-02 20:22:12 -0300

    Borrado de carpetas no necesarias y creacion de nueva clase para la interfaz visual

--- TPParadigmas/src/modelo/dominio/Usuario.java
+++ TPParadigmas/src/modelo/dominio/Usuario.java
@@ -10,5 +10,66 @@ package modelo.dominio;
  * @author franc
  */
 public class Usuario {
+    private int legajo;
+    private String nombre;
+    private String apellido;
+    private String tipoUsuario;
+
+    /**
+     * @return the legajo
+     */
+    public int getLegajo() {
+        return legajo;
+    }
+
+    /**
+     * @param legajo the legajo to set
+     */
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the tipoUsuario
+     */
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    /**
+     * @param tipoUsuario the tipoUsuario to set
+     */
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+    
     
 }
